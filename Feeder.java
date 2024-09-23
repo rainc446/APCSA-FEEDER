fc33b4510c0ee45dbadadc77d4b20253cef1182c
Rain Chen <rainc8@nycstudents.net>
2024-09-23 12:03:55 -0400

    Updated

--- Feeder.java
+++ Feeder.java
@@ -4,14 +4,28 @@ public class Feeder {
      * The amount of food, in grams, currently in the bird feeder; initialized
      * in the constructor and always greater than or equal to zero
      */
+
     private int currentFood;
 
+    public Feeder(int c)
+    {
+        currentFood = c;
+    }
+
+    public Feeder() {}
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
     public void simulateOneDay(int numBirds)
     {
+        if((Math.random()*10) < .05) {currentFood = 0;}
+        else
+        {
+            int eaten = (int) (Math.random() *41 ) +10;
+            currentFood -= eaten*numBirds;
+            if(currentFood < 0) {currentFood = 0;}
+        }
     }
 
     /**
@@ -21,11 +35,42 @@ public class Feeder {
      */
     public int simulateManyDays(int numBirds, int numDays)
     {
-        return 0;
+        for(int days = 0; days < numDays; days++)
+        {
+
+            if((Math.random()*10) < .05)
+            {
+                currentFood = 0;
+                System.out.println("Day " + days + " Amount of Food:" + currentFood);
+                System.out.println("A bear has found the feeder on day " + days);
+                return currentFood; //ends simulation
+            }
+
+
+            else
+            {
+                int eaten = (int) (Math.random() *41 ) +10;
+                currentFood -= eaten*numBirds;
+                if(currentFood < 0)
+                {
+                    currentFood = 0;
+                }
+
+                System.out.println("Day " + days + " Amount of Food:" + currentFood); //doesn't work/print
+            }
+
+        }
+        return currentFood;
     }
     public int getCurrentFood()
     {
         return currentFood;
+    }
+
+    //doesn't work
+    public String toString()
+    {
+        return "Current Food: " + currentFood;
     }
 
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,23 @@ public class Main {
 
     public static void main(String[] args)
     {
+        /*
         Feeder f = new Feeder();
         System.out.println(f);
+        System.out.println(f.getCurrentFood());
+
+        Feeder g = new Feeder(500);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+        */
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+        //
+        Feeder f = new Feeder(2400);
+        f.simulateOneDay(10);
+        System.out.println(f.getCurrentFood());
+
+
     }
 }
